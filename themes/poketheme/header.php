469d7db9a58d68458d3d172332aa690dd163f2da
Kortney <kortney_foley@hotmail.com>
2024-04-02 18:45:10 -0300

    added custom functionality for the menu (navbar), added a header image placeholder

--- themes/poketheme/header.php
+++ themes/poketheme/header.php
@@ -7,3 +7,12 @@
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
         <?php wp_head(); ?>
     </head>
+    <body>
+        <header class="my-logo">
+            <h1>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                    <img src="<?php echo get_template_directory_uri(); ?>/Images/logo.png" alt="Site Logo" width="120px" height="120px">
+                </a>
+            </h1>
+        </header>
+        <?php wp_nav_menu( array( 'header-menu' => 'header-menu' ) ); ?>
